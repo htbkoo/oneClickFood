50b31cd73531327112e33be38648cfe54c30770a
htbkoo <h1017760@connect.hku.hk>
2018-03-24 07:36:32 +0800

    App.js - add a simple form QR Code Generator - FoodChainMenu

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            text: "",
+            qrcode: ""
+        };
+
+        this.onTextChange = this.onTextChange.bind(this);
+    }
+
+    onTextChange(e) {
+        this.setState({text: e.target.value});
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="input-text">
+                    <form>
+                        <label>
+                            Text to transform:
+                            <input type="text" name="text" value={this.state.text}
+                                   onChange={this.onTextChange}/>
+                        </label>
+                    </form>
+                </div>
+                <div className="qrcode">
+                    <img src={this.state.qrcode}/>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
