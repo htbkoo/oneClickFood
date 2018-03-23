b8eac50c256fe4d0b48e8d0ee32ce0828dc899d2
htbkoo <h1017760@connect.hku.hk>
2018-03-24 07:36:47 +0800

    removed unused files QR Code Generator - FoodChainMenu

--- index.test.js
+++
@@ -1,9 +0,0 @@
-var QRCode = require('qrcode');
-
-describe("test", function () {
-    it('should test', function () {
-        QRCode.toDataURL('I am a pony!', function (err, url) {
-            console.log(url)
-        });
-    });
-});
