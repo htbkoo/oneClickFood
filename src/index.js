773070ccf59d1d541a7df47ca10076649937e277
htbkoo <h1017760@connect.hku.hk>
2018-03-24 08:02:41 +0800

    wrapped component with material-ui <MuiThemeProvider/> QR Code Generator - FoodChainMenu

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+
+import './css/index.css';
+
+ReactDOM.render(
+    <MuiThemeProvider>
+        <App/>
+    </MuiThemeProvider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
